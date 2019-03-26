42e305d79730461559c2ee2f9e48d7899537e3fc
John Hill <john.m.hill91@gmail.com>
2019-03-26 20:37:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_the_cheese(newArr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  newArr.find 
+  newArr.find do |ingred|
+    cheese_type.include?
 end
